7ee64c4822b12375d6287415f896adf1be49abfa
Brockston King <brockstonking@Brockstons-MacBook-Pro.local>
2019-07-08 12:31:17 -0600

    working

--- debug name variable.js
+++ debug name variable.js
@@ -1,3 +1,6 @@
 //This code should store "codewa.rs" as a variable called "name" but it's not working. Can you figure out why?
 
-// ???
+const store = () => {
+    const name = 'codewa.rs'
+    return name
+}
--- decode the morse code.js
+++ decode the morse code.js
@@ -18,4 +18,70 @@
 // Good luck!
 //
 
-// ???
+var textandmorse = [
+    {"letter":'A', 'morse':".-"},
+    {"letter":'B', 'morse':"-..."},
+    {"letter":'C', 'morse':"-.-."},
+    {"letter":'D', 'morse':"-.."},
+    {"letter":'E', 'morse':"."},
+    {"letter":'F', 'morse':"..-."},
+    {"letter":'G', 'morse':"--."},
+    {"letter":'H', 'morse':"...."},
+    {"letter":'I', 'morse':".."},
+    {"letter":'J', 'morse':".---"},
+    {"letter":'K', 'morse':"-.-"},
+    {"letter":'L', 'morse':".-.."},
+    {"letter":'M', 'morse':"--"},
+    {"letter":'N', 'morse':"-."},
+    {"letter":'O', 'morse':"---"},
+    {"letter":'P', 'morse':".--."},
+    {"letter":'Q', 'morse':"--.-"},
+    {"letter":'R', 'morse':".-."},
+    {"letter":'S', 'morse':"..."},
+    {"letter":'T', 'morse':"-"},
+    {"letter":'U', 'morse':"..-"},
+    {"letter":'V', 'morse':"...-"},
+    {"letter":'W', 'morse':".--"},
+    {"letter":'X', 'morse':"-..-"},
+    {"letter":'Y', 'morse':"-.--"},
+    {"letter":'Z', 'morse':"--.."},
+    {"letter":'1', 'morse':".----"},
+    {"letter":'2', 'morse':"..---"},
+    {"letter":'3', 'morse':"...--"},
+    {"letter":'4', 'morse':"....-"},
+    {"letter":'5', 'morse':"....."},
+    {"letter":'6', 'morse':"-...."},
+    {"letter":'7', 'morse':"--..."},
+    {"letter":'8', 'morse':"---.."},
+    {"letter":'9', 'morse':"----."},
+    {"letter":'0', 'morse':"-----"},
+    {"letter":'.', 'morse':".-.-.-"},
+    {"letter":',', 'morse':"--..--"},
+    {"letter":':', 'morse':"---..."},
+    {"letter":'?', 'morse':"..--.."},
+    {"letter":'\'', 'morse':".----."},
+    {"letter":'-', 'morse':"-....-"},
+    {"letter":'/', 'morse':"-..-."},
+    {"letter":'(', 'morse':"-.--.-"},
+    {"letter":')', 'morse':"-.--.-"},
+    {"letter":'"', 'morse':".-..-."},
+    {"letter":'@', 'morse':".--.-."},
+    {"letter":'=', 'morse':"-...-"},
+    {"letter":' ', 'morse':"/"}
+    ];
+
+const decodeMorse = (str) => {
+    const words = str.split('   ')
+    return words.reduce( (r, e, i) => {
+        r.push(e.split(' ').reduce( (r1, e1, i1) => {
+            r1.push(textandmorse.reduce( (run, el, ind) => {
+                if (el.morse === e1){
+                    run.push(el.letter)
+                } 
+                return run
+            }, []))
+            return r1
+        }, []).join(''))
+        return r
+    }, []).join(' ')
+}
